6cf0ec3d5b9674880b860002d0a756b7729b9416
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-14 22:51:11 +0300

    Add not found component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { noAuthGuard } from './core/guards/noAuth.guard';
+import { NotFound } from './shared/components/not-found/not-found';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('../app/features/home/home').then(comp => comp.Home) },
     { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login), canActivate: [noAuthGuard] },
     { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register), canActivate: [noAuthGuard] },
-    { path: 'themes', loadComponent: () => import('./features/themes/themes/themes').then(comp => comp.Themes) }
+    { path: 'themes', loadComponent: () => import('./features/themes/themes/themes').then(comp => comp.Themes) },
+    { path: '**', component: NotFound }
 ];
