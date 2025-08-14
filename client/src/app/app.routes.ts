5cae054280068515f7810dfffd7cc01707251559
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-14 18:40:05 +0300

    Add auth guards

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { noAuthGuard } from './core/guards/noAuth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('../app/features/home/home').then(comp => comp.Home) },
-    { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login) },
-    { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register) }
+    { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login), canActivate: [noAuthGuard] },
+    { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register), canActivate: [noAuthGuard] }
 ];
